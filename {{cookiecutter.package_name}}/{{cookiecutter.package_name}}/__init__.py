f245edd8ead34c1738ea7a569c0849c19bd6962a
Dave Shawley <daveshawley@gmail.com>
2015-10-09 18:15:41 -0700

    Add docker bootstrapping.

--- {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/__init__.py
@@ -1,2 +1,8 @@
+try:
+    from {{cookiecutter.project_name}}.app import run
+except ImportError as exc:
+    def run(*args, **kwargs):
+        raise exc
+
 version_info = (0, 0, 0)
 __version__ = '.'.join(str(v) for v in version_info[:3])
---
+++ {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/app.py
@@ -0,0 +1,5 @@
+import sys
+
+
+def run():
+    sys.exit(0)
