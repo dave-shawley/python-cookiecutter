5a71bc914e6332931b8f6cc87a06dfa33caba5bb
Dave Shawley <daveshawley@gmail.com>
2015-10-10 08:29:05 -0700

    Make app.run do something useful.

--- {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/app.py
+++ {{cookiecutter.package_name}}/{{cookiecutter.package_name}}/app.py
@@ -1,5 +1,13 @@
+import signal
 import sys
 
 
 def run():
+    def _handler(signo, frame):
+        print('Signal', signo, 'received, exiting')
+
+    signal.signal(signal.SIGINT, _handler)
+    signal.signal(signal.SIGTERM, _handler)
+    print('Running, press Ctrl+C to exit.')
+    signal.pause()
     sys.exit(0)
